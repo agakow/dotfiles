dbd0b18bf555b596326fa71c9e5f5e1f28cc239e
Aga Kowalczuk <kowalczuk.a93@gmail.com>
2020-03-01 16:20:46 +0000

    Fixes

--- bin/atom-backup.sh
+++ bin/atom-backup.sh
@@ -1,10 +1,6 @@
 #!/bin/sh
-#
-# Usage: atom-package-backup
-#
-# Saves a list of your currently installed atom packages to
-# ~$DOTFILES/atom.symlink/packages.txt suitable for install
-# via atom-package-install
+
+# Backup install packages to file
 
 set -e
 
--- zsh/.zshrc
+++ zsh/.zshrc
@@ -1,5 +1,5 @@
 export PROJECTS=$HOME/Projects
-export DOTFILES=$PROJECTS/dotfiles
+export DOTFILES=$HOME/.dotfiles
 export ZSH="$HOME/.oh-my-zsh"
 
 ZSH_THEME="awesomepanda"
